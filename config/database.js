dcbf8611169ac6eaff3b8ee4e01865e7fb17b1e6
Kevin Salazar <kskevin170@yahoo.com>
2016-11-18 16:20:13 -0800

    Adds monogodb to app

---
+++ config/database.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+
+var env = require('./environment');
+
+var dbUri = env.MONGOLAB_URI ||
+        'mongodb://localhost/' + env.SAFE_TITLE;
+if(!env.MONGOLAB_URI) {
+  require('net').connect(27017, 'localhost').on('err', function() {
+    console.log("You must bow.")
+    process.exit(0);
+  })
+}
+
+mongoose.connect(dbUri);
+
+module.exports = mongoose;
