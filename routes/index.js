33b75741fd71420fb8c22132378e7e88be21fdf6
viviana rishe <vivianacuri@gmail.com>
2016-11-21 14:16:23 -0800

    adds content to home page

--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,7 @@ router.get('/', function(req, res) {
   res.render('index', {user: req.user});
 });
 
+
 router.get('/auth/google', passport.authenticate(
   'google',
   { scope: ['profile', 'email'] }
