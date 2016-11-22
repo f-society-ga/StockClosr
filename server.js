10b3b381376194099ba98f9de996aa842a0b6b3c
Andrew Gonzalez <andrew.gonzalez@gmail.com>
2016-11-22 10:06:55 -0800

    adds logic to populate watchlist table

--- server.js
+++ server.js
@@ -24,11 +24,11 @@ app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(logger('dev'));
+app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
   secret: 'WDIRocks!',
   resave: false,
