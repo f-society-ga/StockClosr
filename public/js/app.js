f95de51e4149670ae53343fc2b358ba9d7e5de93
Andrew Gonzalez <andrew.gonzalez@gmail.com>
2016-11-21 16:34:10 -0800

    populates watchlist ver 1

--- public/js/app.js
+++ public/js/app.js
@@ -16,3 +16,19 @@
 // }).done(function(stock) {
 //   $('#ul').append(stock.LastPrice);
 // });
+
+
+$(document).ready(function(){
+  $.ajax({
+    url: "http://dev.markitondemand.com/Api/v2/quote/json?symbol=AAPL",
+    type: "GET",
+    dataType: "json"
+  }).done(function(stock) {
+    $('#tableSymbol').html(stock.Symbol);
+    $('#tableCompany').html(stock.Name);
+    $('#tableOpeningPrice').html('Open: ' + stock.Open);
+    $('#tableCurrent').html('Current Price: ' + stock.LastPrice);
+    $('#tableHigh').html('High: ' + stock.High);
+    $('#tableLow').html('Low: ' + stock.Low);
+  });
+});
