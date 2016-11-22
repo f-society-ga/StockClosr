d706551cd4a1c7d329bfd7641f0fa2ff3ba36af2
viviana rishe <vivianacuri@gmail.com>
2016-11-21 16:51:17 -0800

    deploy version

--- config/database.js
+++ config/database.js
@@ -1,11 +1,18 @@
 var mongoose = require('mongoose');
 
-mongoose.connect(process.env.DATABASE_URL);
+// Use different database URIs based on whether an env var exists.
+//process.env.LOCAL_DB is in the .env file look for key, if we have heroku it will look for MONGODB_URI
+var dbUri = process.env.MONGODB_URI ||
+            'mongodb://localhost/' + process.env.DATABASE_URL;
 
-// database connection event
+if (!process.env.MONGODB_URI) {
+  // check that MongoD is running...
+  require('net').connect(27017, 'localhost').on('error', function() {
+    console.log("YOU MUST BOW BEFORE THE MONGOD FIRST, MORTAL!");
+    process.exit(0);
+  });
+}
 
-mongoose.connection.once('open', function(cb) {
-  console.log(`Mongoose connected to: ${process.env.DATABASE_URL}`);
-});
+mongoose.connect(dbUri);
 
 module.exports = mongoose;
