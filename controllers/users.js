1b748931e18bdbbdf3d5bec40eea0b4feaf21376
Kevin Salazar <kskevin170@yahoo.com>
2016-11-22 11:26:13 -0800

    adds user showpage and buttons

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,9 @@
 var User = require('../models/user');
+var session = require('express-session')
 
 module.exports = {
-  index: index
+  index: index,
+  userShow: userShow
 }
 
 function index(req, res) {
@@ -9,4 +11,10 @@ function index(req, res) {
     if(err) return res.status(err.statusCode || 500).json(err)
     res.render(users)
   });
-}; 
+};
+function userShow(req, res) {
+  User.find({}, function(err, users) {
+    if(err) return res.status(err.statusCode || 500).json(err)
+    res.render('pages/userShow', {user: req.user})
+  })
+}
